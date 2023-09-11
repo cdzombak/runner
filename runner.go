dc6f51c1f4f3241568dbada0057a0c66e404963b
Chris Dzombak <chris@dzombak.com>
2023-09-11 11:40:57 -0400

    extract file utils; util naming convention

--- runner.go
+++ runner.go
@@ -281,25 +281,3 @@ func main() {
 		}
 	}
 }
-
-func writeToFile(filename string, data string) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.WriteString(file, data)
-	if err != nil {
-		return err
-	}
-	return file.Sync()
-}
-
-func removeBadFilenameChars(filename string) string {
-	badChars := []string{"/", "\\", "?", "%", "*", ":", "|", "\"", "'", "<", ">", ".", " "}
-	for _, v := range badChars {
-		filename = strings.ReplaceAll(filename, v, "-")
-	}
-	return filename
-}
