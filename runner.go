1a6bb1a754c58f379eb7ade8981778978b55f653
Chris Dzombak <chris@dzombak.com>
2023-09-11 09:28:56 -0400

    add support for running as another user

--- runner.go
+++ runner.go
@@ -8,8 +8,11 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/user"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -19,6 +22,7 @@ func usage() {
 	fmt.Printf("Usage: %s [OPTIONS] -- /path/to/program --program-args\n", filepath.Base(os.Args[0]))
 	fmt.Printf("Run the given program, only printing output to stdout/stderr if the program exits with an error.\n")
 	fmt.Printf("Optionally, all output is logged to a user-configurable directory.\n")
+	fmt.Printf("If run as root or with CAP_SETUID and CAP_SETGID, the program can be run as a different user.\n")
 	fmt.Printf("\nOptions:\n")
 	flag.PrintDefaults()
 	fmt.Printf("\nVersion:\n  runner version %s\n", version)
@@ -38,6 +42,9 @@ func main() {
 	logDir := flag.String("log-dir", "", "The directory to write run logs to. Can also be set by the RUNNER_LOG_DIR environment variable; this flag overrides the environment variable.")
 	workDir := flag.String("work-dir", "", "Set the working directory for the program.")
 	retries := flag.Int("retries", 0, "If the command fails, retry it this many times.")
+	asUser := flag.String("user", "", "Run the program as the given user. (If provided, runner must be run as root or with CAP_SETUID and CAP_SETGID.)")
+	asUID := flag.Int("uid", -1, "Run the program as the given UID. (If provided, runner must be run as root or with CAP_SETUID.)")
+	asGID := flag.Int("gid", -1, "Run the program as the given GID. (If provided, runner must be run as root or with CAP_SETUID.)")
 	printVersion := flag.Bool("version", false, "Print version and exit.")
 	flag.Usage = usage
 	flag.Parse()
@@ -64,6 +71,40 @@ func main() {
 		healthyExitCodes = []int{0}
 	}
 
+	if *asUser != "" && (*asUID != -1 || *asGID != -1) {
+		log.Fatalf("Cannot specify both -user and -uid/-gid")
+	}
+	if *asUser != "" {
+		u, err := user.Lookup(*asUser)
+		if err != nil {
+			log.Fatalf("Failed to lookup user %s: %s", *asUser, err)
+		}
+		uid, err := strconv.ParseInt(u.Uid, 10, 32)
+		if err != nil {
+			log.Fatalf("Failed to parse UID %s as integer: %s", u.Uid, err)
+		}
+		gid, err := strconv.ParseInt(u.Gid, 10, 32)
+		if err != nil {
+			log.Fatalf("Failed to parse UID %s as integer: %s", u.Uid, err)
+		}
+		*asUID = int(uid)
+		*asGID = int(gid)
+	}
+	var sysProcAttr *syscall.SysProcAttr = nil
+	if *asUID != -1 || *asGID != -1 {
+		sysProcAttr = &syscall.SysProcAttr{}
+		sysProcAttr.Credential = &syscall.Credential{}
+		if *asUID != -1 {
+			sysProcAttr.Credential.Uid = uint32(*asUID)
+		}
+		if *asGID != -1 {
+			sysProcAttr.Credential.Gid = uint32(*asGID)
+		}
+		log.Printf("Program will run as UID %d, GID %d", sysProcAttr.Credential.Uid, sysProcAttr.Credential.Gid)
+	}
+		}
+	}
+
 	triesRemaining := 1 + *retries
 	programOutput := ""
 	var startTime, endTime time.Time
@@ -80,6 +121,9 @@ func main() {
 
 		startTime = time.Now()
 		cmd := exec.Command(programName, programArgs...)
+		if sysProcAttr != nil {
+			cmd.SysProcAttr = sysProcAttr
+		}
 		cmd.Dir = *workDir
 		cmdOut, err := cmd.CombinedOutput()
 		endTime = time.Now()
