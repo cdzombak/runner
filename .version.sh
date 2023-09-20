11b7943610eb7fc1a63be389782525bf9140d4cc
Chris Dzombak <chris@chrisdzombak.net>
2023-09-20 16:00:58 -0400

    fix version script

--- .version.sh
+++ .version.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 if [ -z "$(git tag --points-at HEAD)" ]; then
 	git describe --always --long --dirty | sed 's/^v//'
 else
-	git tag --points-at HEAD | sed 's/^v//'
+	git tag --points-at HEAD | sed 's/^v//' | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | tail -n 1
 fi
